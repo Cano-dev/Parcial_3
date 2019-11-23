c454efe5953a7d780b26631244355acf24d3f034
Luis Gomez <00405018@uca.edu.sv>
2019-11-23 14:46:10 -0600

    Creacion de archivos .pug y correccion de errores menores

---
+++ APIREST/routes/products.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const products = [
+    {
+        masa: "Arroz",
+        precio: 0.50,
+        especialidad: "Revueltas",
+        tamanio: "Grande"
+    },
+    {
+        masa: "Maiz",
+        precio: 1,
+        especialidad: "Loca",
+        tamanio: "Grande"
+    }
+]
+
+router.get('/', function(req, res){
+    res.render("products", { products });
+});
+
+module.exports = router;
+
